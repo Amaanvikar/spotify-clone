b75b78d040a274e1cd4553ad02d54d8541f7b282
Amaanvikar <amaanvikar8313@gmail.com>
2026-04-06 12:28:11 +0530

    transfer push

--- server.js
+++ server.js
@@ -2,11 +2,11 @@ require('dotenv').config();
 const app = require('./src/app');
 const connectDB = require('./src/db/db');
 
-
-connectDB();
-
-
-
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
-})
+connectDB().then(() => {
+    app.listen(8080, () => {
+        console.log('Server is running on port ');
+    });
+}).catch((err) => {
+    console.error('MongoDB connection failed:', err.message);
+    process.exit(1);
+});
